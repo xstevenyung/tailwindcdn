3038b342f022106562fcb4668ccc71993ceb3c1a
Steven Yung <stevenyung@fastmail.com>
2022-07-07 15:13:56 +0200

    add redis cache option

--- apps/web/routes/[file].tsx
+++ apps/web/routes/[file].tsx
@@ -3,16 +3,43 @@ import { h } from "preact";
 // import Counter from "../islands/Counter.tsx";
 import { Handlers } from "$fresh/server.ts";
 import { extname } from "https://deno.land/std/path/mod.ts";
+import { connect } from "https://deno.land/x/redis/mod.ts";
 
 export const handler: Handlers = {
   async GET(_req, ctx) {
     const base64 = ctx.params.file.replace(extname(ctx.params.file), "");
+
+    let redis;
+    if (Deno.env.get("REDIS_CACHE_ENABLED") === "true") {
+      console.log("redis cache enabled");
+
+      redis = await connect({
+        hostname: Deno.env.get("REDIS_HOST") as string,
+        port: Deno.env.get("REDIS_PORT"),
+        tls: Deno.env.get("REDIS_TLS") === "true",
+        password: Deno.env.get("REDIS_PASSWORD"),
+      });
+    }
+
+    if (redis) {
+      const cachedStylesheet = await redis.get(base64);
+      if (cachedStylesheet) {
+        return new Response(cachedStylesheet, {
+          headers: { "Content-Type": "text/css" },
+        });
+      }
+    }
+
     const stylesheet = await fetch(Deno.env.get("COMPILER_URL") as string, {
       method: "POST",
       body: JSON.parse(atob(base64)),
     }).then(
       (response) => response.text(),
     );
+
+    if (redis) {
+      redis.set(base64, stylesheet);
+    }
 
     return new Response(stylesheet, {
       headers: {
@@ -21,20 +48,3 @@ export const handler: Handlers = {
     });
   },
 };
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <img
-//         src="/logo.svg"
-//         height="100px"
-//         alt="the fresh logo: a sliced lemon dripping with juice"
-//       />
-//       <p>
-//         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-//         file, and refresh.
-//       </p>
-//       <Counter start={3} />
-//     </div>
-//   );
-// }
